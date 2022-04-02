9a1fe628319a822c24338f1061da83c6a512431c
Gabriel Kositsky <gabiks318@gmail.com>
2022-04-02 16:36:11 +0300

    Changed server to listen in loop

--- server.py
+++ server.py
@@ -10,17 +10,16 @@ if __name__ == '__main__':
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen()
-    connection, address = server_socket.accept()
+    while True:
+        connection, address = server_socket.accept()
 
-    # Send data to connection
-    data = connection.recv(1024)
+        # Send data to connection
 
-    with open("example.html", "r") as f:
-        content = f.read()
+        with open("example.html", "r") as f:
+            content = f.read()
 
-    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{content}"
-    connection.send(response.encode())
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{content}"
+        connection.send(response.encode())
 
-    # Close sockets
-    connection.close()
-    server_socket.close()
+        # Close socket
+        connection.close()
